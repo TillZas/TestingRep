874816293b215209d7c07e773edd0a9755639947
Sergey Gulyaev <TillZas@gmail.com>
2017-07-30 14:11:14 +0300

    Глава 10 + Чат
    Спроси об описании функций!! Спроси о проблеме с функциями Is. Спроси о
    проблемах с разметкой!

---
+++ DataBasesAndModels/DataBasesAndModels/Controllers/ChatController.cs
@@ -0,0 +1,86 @@
+﻿using DataBasesAndModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataBasesAndModels.Controllers
+{
+    public class ChatController : Controller
+    {
+        TownContext database = new TownContext();
+
+        int messageBufferSize = 100;
+        int userMaxAmount = 10;
+        int userMaxPingSec = 30;
+
+        static ChatModel chatModel;
+
+        public ActionResult Index(string user, bool? logOn,bool? logOff, string chatMessage, DateTime? chatMessageTimestamp)
+        {
+            Debug.WriteLine("Index called!!!");
+            try
+            {
+
+                if (chatModel == null) chatModel = new ChatModel(messageBufferSize,userMaxAmount);
+                 if (!Request.IsAjaxRequest())
+                {
+                    Debug.WriteLine("Regular request");
+                    List<SelectListItem> peopleList;
+
+                    peopleList = database.Characters
+                    .Select(c => new SelectListItem()
+                    {
+                        Text = (c.Name + " " + c.Surname),
+                        Value = (c.Name + c.Surname)
+                    }).ToList();
+
+                    peopleList.RemoveAll(el => chatModel.Users.FirstOrDefault(usr => usr.Name == el.Value) != null);
+                    
+                    ViewBag.People = new SelectList(peopleList, "Value", "Text"); ;
+                    return View(chatModel);
+                }
+                else if(logOn != null && (bool)logOn)
+                {
+                    Debug.WriteLine("Logon request");
+                    int logInResult = chatModel.AddUser(user);
+                    if (logInResult == ChatModel.NULL_POINTER_RESULT) throw new Exception("Пустое имя пользователя.");
+                    if (logInResult == ChatModel.ALREADY_CONNECTED_RESULT) throw new Exception("Данный никнейм уже занят.");
+                    if (logInResult == ChatModel.NO_MORE_ROOM_RESULT) throw new Exception("Чат заполнен.");
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else if(logOff != null && (bool)logOff)
+                {
+                    Debug.WriteLine("Logoff request");
+                    chatModel.LogOff(user);
+                    return PartialView("ChatRoom", chatModel);
+                }
+                else
+                {
+                    Debug.WriteLine("Chat request");
+                    ChatUser chu = chatModel.TouchUser(user);
+
+                    chatModel.RemoveDelayedUsers(userMaxPingSec);
+
+                    if (!string.IsNullOrEmpty(chatMessage))
+                        if (chatMessageTimestamp != null)
+                            chatModel.AddMessage(chatMessage, chu, (DateTime)chatMessageTimestamp);
+                        else
+                            chatModel.AddMessage(chatMessage, chu);
+
+                    return PartialView("History", chatModel);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                return Content(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ DataBasesAndModels/DataBasesAndModels/Models/ChatModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataBasesAndModels.Models
+{
+    public class ChatModel
+    {
+        public List<ChatUser> Users;
+        private List<ChatMessage> Messages;
+
+        private int cacheLimit = 0;
+        private int userMaxAmount_ = 0;
+
+        public ChatModel(int messageCacheLimit,int userMaxAmount)
+        {
+            Users = new List<ChatUser>();
+            Messages = new List<ChatMessage>();
+
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат стартовал в " + DateTime.Now,
+            });
+
+            userMaxAmount_ = userMaxAmount;
+        }
+
+
+        public void AddMessage(string Message,ChatUser User)
+        {
+            Messages.Add(new ChatMessage()
+            {
+                User = User,
+                Text = Message,
+                Date = DateTime.Now
+            });
+            Messages.Sort(new MessagesComparer());
+            if (cacheLimit > 0 && Messages.Count > cacheLimit) Messages.RemoveRange(0, Messages.Count - cacheLimit);
+        }
+
+        public void AddMessage(string Message, ChatUser User,DateTime TimeStamp)
+        {
+            Messages.Add(new ChatMessage()
+            {
+                User = User,
+                Text = Message,
+                Date = TimeStamp
+            });
+            
+            Messages.Sort(new MessagesComparer());
+            if (cacheLimit > 0 && Messages.Count > cacheLimit) Messages.RemoveRange(0, Messages.Count - cacheLimit);
+        }
+
+        public List<ChatMessage> GetMessages()
+        {
+            return Messages.FindAll(x => x.Date.ToFileTime()<= DateTime.Now.ToFileTime());
+        }
+
+        /*
+        public bool IsUserConnected(string userName)
+        {
+            return Users.FirstOrDefault(user => user.Name == userName) != null;
+        }
+
+        public bool IsHavePlaceToAnother()
+        {
+            return (userMaxAmount_ <= 0) || (Users.Count < userMaxAmount_);
+        }*/
+
+        public static int NULL_POINTER_RESULT = -3, ALREADY_CONNECTED_RESULT = -1, NO_MORE_ROOM_RESULT = -2;
+
+        //Спроси о том как писать описание функциям
+        public int AddUser(string userName)
+        {
+            if (string.IsNullOrEmpty(userName)) return -3;
+            if (Users.FirstOrDefault(user => user.Name == userName) != null) return -1;
+            if (!((userMaxAmount_ <= 0) || (Users.Count < userMaxAmount_))) return -2;
+            Users.Add(new ChatUser()
+            {
+                Name = userName,
+                LoginTime = DateTime.Now,
+                LastPing = DateTime.Now
+            });
+            AddMessage(userName + " присоеденился к чату.",null);
+            return 0;
+        }
+
+        public void RemoveDelayedUsers(int seconds)
+        {
+            List<ChatUser> toRemove = Users.FindAll(usr => ((TimeSpan)(DateTime.Now - usr.LastPing)).TotalSeconds > seconds);
+            foreach(ChatUser chu in toRemove)
+            {
+                Users.Remove(chu);
+                AddMessage(chu.Name + " потерял соединение.", null);
+            }
+        }
+
+        public void LogOff(string userName)
+        {
+            if(Users.RemoveAll(usr => usr.Name == userName)>0)
+                AddMessage(userName + " вышел.",null);
+        }
+
+        public ChatUser TouchUser(string userName)
+        {
+            ChatUser ch = Users.FirstOrDefault(usr => usr.Name == userName);
+            if (ch == null) return null;
+            ch.LastPing = DateTime.Now;
+            return ch;
+        }
+
+        private class MessagesComparer : IComparer<ChatMessage>
+        {
+            public int Compare(ChatMessage x, ChatMessage y)
+            {
+                
+                return x.Date.CompareTo(y.Date);
+
+            }
+        }
+    }
+
+    public class ChatUser
+    {
+        public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+    }
+
+    public class ChatMessage
+    {
+        public ChatUser User;
+        public DateTime Date = DateTime.Now;
+        public string Text = "";
+    }
+}
